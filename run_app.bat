66d21757c16ec6cc54bda77df17429208f16576c
santhosh-15082003 <santhoshb1503@gmail.com>
2026-09-06 21:38:37 +0530

    Add run_app.bat helper script to launch Streamlit with correct PYTHONPATH

---
+++ run_app.bat
@@ -0,0 +1,7 @@
+@echo off
+title Python ToolKit
+cd /d "D:\Project\Python\Python_ToolKit_Using_Artificial_Intelligence"
+set PYTHONPATH=D:\Lib\site-packages
+echo Starting Python ToolKit (Streamlit)...
+D:\python.exe -m streamlit run Main.py
+pause
